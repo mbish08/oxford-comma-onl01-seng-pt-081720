800263038ef5af7aa3885efc89b30adaf4771511
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 02:44:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,6 @@
 def oxford_comma(array)
-  if array.size 
-array.join
-end
-
-def oxford_comma(array)
-  array.join(" and ")
-end
-
-def oxford_comma(array)
-  array.join(", " "and")
+  if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ")
 end
