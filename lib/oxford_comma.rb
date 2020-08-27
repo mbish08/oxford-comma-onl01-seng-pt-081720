ad317e300515e06890e41f877b143caec2f94801
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 02:55:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,6 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
     elsif array.size == 3
-   arr3 << array.join(", ")
-   arr3.insert(-1, "and")
-   arr3
+    array.insert(-2, "and")
   end
 end
